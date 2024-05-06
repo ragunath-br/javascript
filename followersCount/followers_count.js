a2ae3a3b98227c6de2e5a3cc73b8a91003a17547
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-05-05 22:32:40 -0400

    adds reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,10 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
+function resetCount(){
+    count = 0;
+    alert("Your follower count has been reset");
+    displayCount();
+
+}
